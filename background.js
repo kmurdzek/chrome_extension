2f39d0ed57974c58bd0c1c1fc587fdc5b1c43afd
Melvin Vito <melvin.vito@clover.com>
2023-11-29 11:15:23 -0500

    Added rightClick. Functionality still buggy (not logging to console yet)

--- background.js
+++ background.js
@@ -1,15 +1,49 @@
+chrome.contextMenus.create({
+  id: "summarizeText",
+  title: "Summarize Selected Text",
+  contexts: ["selection"]
+});
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === "scrape") {
     chrome.scripting.executeScript(
-      {
-        target: { tabId: request.tabId },
-        files: ["contentScript.js"]
-      },
-      () => {
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError.message);
+        {
+          target: { tabId: request.tabId },
+          files: ["contentScript.js"]
+        },
+        () => {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+          }
         }
-      }
     );
   }
 });
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId === "summarizeText") {
+    // The user clicked the context menu item, send a message to the content script
+    chrome.tabs.sendMessage(tab.id, { action: "summarize", text: info.selectionText });
+  }
+});
+
+async function summarizeSelectedText() {
+
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  const response = await fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer YOUR_OPENAI_API_KEY"
+    },
+    body: JSON.stringify({
+      prompt: `Summarize the following text:\n\n${window.getSelection().toString()}`,
+      max_tokens: 50
+    })
+  });
+
+  const data = await response.json();
+  const summary = data.choices[0].text.trim();
+
+  return summary;
+}
--- contentScript.js
+++ contentScript.js
@@ -16,3 +16,53 @@ console.log("Title:", title);
 console.log("Body:", body);
 var selectedText = window.getSelection().toString();
 console.log("Selected text:", selectedText);
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "summarize") {
+        const selectedText = request.text; // The highlighted text
+
+        if (selectedText) {
+            // TODO: Integrate with OpenAI API to get the summary
+            console.log("Selected text to summarize:", selectedText);
+        }
+    }
+});
+
+
+// Function to call OpenAI API and get the summary
+function getSummary(text) {
+    const apiKey = 'YOUR_OPENAI_API_KEY'; // Replace with your OpenAI API key
+    const data = {
+        prompt: text,
+        max_tokens: 100, // Adjust based on desired summary length
+        temperature: 0.7
+    };
+
+    fetch('https://your-api-gateway-url.com/endpoint', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(data => {
+            const summary = data.choices[0].text.trim();
+            // Display the summary (replace the alert with a proper UI in the next steps)
+            alert(summary);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+// Listen for messages from the background script
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "summarize") {
+        const selectedText = request.text;
+        if (selectedText) {
+            getSummary(selectedText);
+        }
+    }
+});
