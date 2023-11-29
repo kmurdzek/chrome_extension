f53e4b950089b2fff9b6f301162fb5b61a1dca4a
Murdzek <kacper.murdzek@lm-x31607y9q3.1dc.com>
2023-11-29 07:56:29 -0500

    Remove openai import

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,4 @@
+/*
 import { Configuration, OpenAIApi } from "openai";
 var openaiKey = "PLACEHOLDER";
 const configuration = new Configuration({
@@ -7,6 +8,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 const response = await openai.listModels();
 console.log(response);
+*/
 var title = document.title;
 var body = document.body.innerText;
 
@@ -14,4 +16,3 @@ console.log("Title:", title);
 console.log("Body:", body);
 var selectedText = window.getSelection().toString();
 console.log("Selected text:", selectedText);
-ß
